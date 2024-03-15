3b90edcc9c29a3400ed2386ced40e6fff961df43
Malykh Andrei <xekoro10@test.com>
2024-03-15 22:47:04 +0300

    5_http_server: added server-wrapper for Jobe

--- src/main.py
+++ src/main.py
@@ -1,18 +1,7 @@
 from flask import Flask
-from src.tasks import mailer, uploader
+
+from src.api.routes import api
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
-
-@app.get("/test_tasks_queue")
-def test_tasks_queue():
-    result = mailer.delay()
-    return {"test_tasks_queue": f"{result.status}"}, 200
-
-@app.get("/test_replies_queue")
-def test_replies_queue():
-    result = uploader.delay()
-    return {"test_replies_queue": f"{result.status}"}, 200
+app.register_blueprint(api)
