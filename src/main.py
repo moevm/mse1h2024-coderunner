2708a5396caebf3db23a7a69a3bd85e2d0a4c347
jonx8 <xekoro10@gmail.com>
2024-05-17 19:57:23 +0300

    38_status_route: added celery workers and queues status check

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import requests
 from flask import Flask
 
 from src.api.routes import api
-from src.settings import JOBE_SERVERS
+from src.settings import JOBE_SERVERS, FLOWER_URL
 
 app = Flask(__name__)
 
@@ -11,7 +11,17 @@ app.register_blueprint(api)
 
 @app.get('/status')
 def get_status():
-    result = {"jobe_servers": [], "celery": []}
+    result = {"jobe_servers": [], "celery": {}}
+    try:
+        workers_info = requests.get(FLOWER_URL + "/api/workers", params={"status": 1}).json()
+        result["celery"]["workers"] = dict((k, "active" if v else "not available") for k, v in workers_info.items())
+
+        queues_info = requests.get(FLOWER_URL + "/api/queues/length").json()
+        result["celery"]["queues"] = queues_info["active_queues"]
+
+    except requests.exceptions.RequestException:
+        result["celery"]["flower"] = "not available"
+
     for url in JOBE_SERVERS:
         try:
             status = requests.get(url + "/jobe/index.php/restapi/languages").ok
